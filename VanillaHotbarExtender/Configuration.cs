efedf324ca3df59adbe27b286f82ee991cecc76e
Mustafa Kalash <me@mkalash.com>
2022-10-23 01:14:40 -0400

    Basic functionality - Save/load hotbars with chat commands - Persistent saves - Menu to edit names of or delete hotbars

---
+++ VanillaHotbarExtender/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+using System.Collections.Generic;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+
+namespace VanillaHotbarExtender {
+    [Serializable]
+    public struct HotbarSlotSave {
+        public uint CommandId;
+        public HotbarSlotType CommandType;
+
+        public HotbarSlotSave(uint commandId, HotbarSlotType commandType) {
+            this.CommandId = commandId;
+            this.CommandType = commandType;
+        }
+    }
+
+    [Serializable]
+    public class Configuration : IPluginConfiguration {
+        public int Version { get; set; } = 0;
+
+        public Dictionary<string, HotbarSlotSave[]> Hotbars { get; set; } = new Dictionary<string, HotbarSlotSave[]>();
+
+        // the below exist just to make saving less cumbersome
+        [NonSerialized]
+        private DalamudPluginInterface? PluginInterface;
+
+        public void Initialize(DalamudPluginInterface pluginInterface) {
+            this.PluginInterface = pluginInterface;
+        }
+
+        public void Save() {
+            this.PluginInterface!.SavePluginConfig(this);
+        }
+    }
+}
---
+++ VanillaHotbarExtender/Windows/ConfigWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace VanillaHotbarExtender.Windows;
+
+public class ConfigWindow : Window, IDisposable {
+    public const string NAME = "Vanilla Hotbar Extender";
+    private Configuration Configuration;
+
+    public ConfigWindow(Plugin plugin) : base(ConfigWindow.NAME) {
+        this.Size = new Vector2(600, 800);
+        this.SizeCondition = ImGuiCond.Appearing;
+
+        this.Configuration = plugin.Configuration;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw() {
+        ImGui.Text("Hotbars");
+        ImGui.Separator();
+        ImGui.Spacing();
+        int id = 0;
+        foreach(var hotbar in this.Configuration.Hotbars) {
+            string inputText = hotbar.Key;
+            ImGui.Text("Name");
+            ImGui.SameLine();
+            if(ImGui.InputText($"##{id}", ref inputText, 32) && !String.IsNullOrWhiteSpace(inputText)) {
+                this.Configuration.Hotbars.Remove(hotbar.Key);
+                this.Configuration.Hotbars.Add(inputText, hotbar.Value);
+                this.Configuration.Save();
+            }
+            ImGui.SameLine();
+            if(ImGui.Button($"Delete##{id}")) {
+                this.Configuration.Hotbars.Remove(hotbar.Key);
+                this.Configuration.Save();
+            }
+            id++;
+        }
+    }
+}
