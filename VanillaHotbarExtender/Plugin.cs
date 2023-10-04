68d4ff4720f9076591d2b4df2cb7ce87ea3f580f
Mustafa Kalash <me@mkalash.com>
2023-10-04 00:15:16 -0400

    Updates to API 9. Adds clear command and hotbar file writing.

--- VanillaHotbarExtender/Plugin.cs
+++ VanillaHotbarExtender/Plugin.cs
@@ -1,15 +1,12 @@
-﻿using Dalamud.Game.Command;
-using Dalamud.IoC;
+﻿using Dalamud.IoC;
 using Dalamud.Plugin;
-using Dalamud.Game.Gui;
-using System.IO;
-using System.Reflection;
-using System.Collections.Generic;
+using Dalamud.Plugin.Services;
 using System;
 using Dalamud.Interface.Windowing;
 using VanillaHotbarExtender.Windows;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using Dalamud.Game.Command;
 
 namespace VanillaHotbarExtender {
     public unsafe sealed class Plugin : IDalamudPlugin {
@@ -17,32 +14,40 @@ namespace VanillaHotbarExtender {
         private const string COMMAND = "/vhe";
         private const string SAVE_COMMAND = "save";
         private const string LOAD_COMMAND = "load";
+        private const string CLEAR_COMMAND = "clear";
         private const int HOTBAR_SIZE = 12;
         private const int HOTBAR_COUNT = 18;
-        private HotBars* Hotbars = &Framework.Instance()->UIModule->GetRaptureHotbarModule()->HotBar;
+        private RaptureHotbarModule* raptureHotbarModule = Framework.Instance()->UIModule->GetRaptureHotbarModule();
+        private ConfigWindow configWindow { get; init; }
 
         private DalamudPluginInterface PluginInterface { get; init; }
-        private CommandManager CommandManager { get; init; }
+        private ICommandManager CommandManager { get; init; }
         public Configuration Configuration { get; init; }
-        public ChatGui ChatGui { get; init; }
+        public IChatGui ChatGui { get; init; }
+        public IClientState ClientState { get; init; }
         public WindowSystem WindowSystem = new("VanillaHotbarExtender");
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] CommandManager commandManager,
-            [RequiredVersion("1.0")] ChatGui chatGui) {
+            [RequiredVersion("1.0")] ICommandManager commandManager,
+            [RequiredVersion("1.0")] IChatGui chatGui,
+            [RequiredVersion("1.0")] IClientState clientState    
+        ) {
             this.PluginInterface = pluginInterface;
             this.CommandManager = commandManager;
             this.ChatGui = chatGui;
+            this.ClientState = clientState;
 
             this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
             this.Configuration.Initialize(this.PluginInterface);
 
-            WindowSystem.AddWindow(new ConfigWindow(this));
+            configWindow = new ConfigWindow(this);
+            WindowSystem.AddWindow(configWindow);
 
             this.CommandManager.AddHandler(COMMAND, new CommandInfo(OnCommand) {
                 HelpMessage = "Open plugin window\n" +
-                              "/vhe {save | load} {hotbar number} {save name} → Save or load a hotbar"
+                              "/vhe {save | load} {hotbar number} {save name} → Save or load a hotbar\n" +
+                              "/vhe clear {hotbar number} → Clear a hotbar\n"
             });
 
             this.PluginInterface.UiBuilder.Draw += DrawUI;
@@ -54,36 +59,27 @@ namespace VanillaHotbarExtender {
             this.CommandManager.RemoveHandler(COMMAND);
         }
 
-        private HotBarSlots* GetHotbar(int hotbarNumber) {
-            if (hotbarNumber < 1 || hotbarNumber > HOTBAR_COUNT) {
-                throw new System.ArgumentOutOfRangeException(nameof(hotbarNumber), $"Hotbar number must be between 1 and {HOTBAR_COUNT}.");
-            }
-
-            return &(*this.Hotbars)[hotbarNumber - 1]->Slot;
-        }
-
-        private void SetHotbar(int hotbarNumber, HotbarSlotSave[] hotbar) {
-            if (hotbarNumber < 1 || hotbarNumber > HOTBAR_COUNT) {
-                throw new System.ArgumentOutOfRangeException(nameof(hotbarNumber), $"Hotbar number must be between 1 and {HOTBAR_COUNT}.");
-            }
-
-            HotBarSlots* gameHotbar = GetHotbar(hotbarNumber);
-            for(int i = 0; i < HOTBAR_SIZE; i++) {
-                HotBarSlot* gameSlot = (*gameHotbar)[i];
-                gameSlot->Set(hotbar[i].CommandType, hotbar[i].CommandId);
-            }
-        }
-
         private bool validateArguments(string[] args) {
-            if(args.Length < 3) {
-                this.ChatGui.PrintError("Invalid arguments. Usage: /vhe {save | load} {hotbar number} {save name}");
+            var usage = "Usage: /vhe clear {hotbar number}";
+            var requiredArgs = 2;
+
+            if(args[0] == SAVE_COMMAND || args[0] == LOAD_COMMAND) {
+                usage = " Usage: /vhe {save | load} {hotbar number} {save name}";
+                requiredArgs = 3;
+            } else if(args[0] != CLEAR_COMMAND) {
+                this.ChatGui.PrintError("Invalid command. Usage: /vhe [save | load | clear]");
                 return false;
             }
 
-            int hotbarNumber;
-            bool isNum = int.TryParse(args[1], out hotbarNumber);
+            if (args.Length < requiredArgs) {
+                this.ChatGui.PrintError("Invalid arguments." + usage);
+                return false;
+            }
+
+            uint hotbarNumber;
+            bool isNum = uint.TryParse(args[1], out hotbarNumber);
             if(!isNum) {
-                this.ChatGui.PrintError("Invalid hotbar number. Usage: /vhe {save | load} {hotbar number} {save name}");
+                this.ChatGui.PrintError("Invalid hotbar number." + usage);
                 return false;
             }
             if(hotbarNumber < 1 || hotbarNumber > HOTBAR_COUNT) {
@@ -97,43 +93,58 @@ namespace VanillaHotbarExtender {
         private void OnCommand(string command, string args) {
             args = args.ToLower();
             if(String.IsNullOrEmpty(args)) {
-                WindowSystem.GetWindow(ConfigWindow.NAME)!.IsOpen = true;
+                configWindow.IsOpen = true;
             } else {
                 string[] argsList = args.Split(" ");
-                if(argsList[0] == SAVE_COMMAND) {
-                    if(!validateArguments(argsList)) {
-                        return;
-                    }
+                if (!validateArguments(argsList)) {
+                    return;
+                }
 
-                    int hotbarNumber = int.Parse(argsList[1]);
+                if(argsList[0] == SAVE_COMMAND) {
+                    
+                    uint hotbarNumber = uint.Parse(argsList[1]) - 1;
                     string saveName = String.Join(" ", argsList[2..]);
 
-                    HotBarSlots* gameHotbar = GetHotbar(hotbarNumber);
                     HotbarSlotSave[] saveHotbars = new HotbarSlotSave[HOTBAR_SIZE];
-                    for(int i = 0; i < HOTBAR_SIZE; i++) {
-                        HotBarSlot* slot = (*gameHotbar)[i];
+                    for(uint i = 0; i < HOTBAR_SIZE; i++) {
+                        HotBarSlot* slot = raptureHotbarModule->GetSlotById(hotbarNumber, i);
                         saveHotbars[i] = new HotbarSlotSave(slot->CommandId, slot->CommandType);
                     }
 
                     this.Configuration.Hotbars[saveName] = saveHotbars;
                     this.Configuration.Save();
-                    this.ChatGui.Print("Hotbar saved.");
+                    this.ChatGui.Print("Hotbar \"" + (hotbarNumber + 1) + "\" saved as \"" + saveName + "\".");
                 } else if(argsList[0] == LOAD_COMMAND) {
                     if(!validateArguments(argsList)) {
                         return;
                     }
 
-                    int hotbarNumber = int.Parse(argsList[1]);
+                    uint hotbarNumber = uint.Parse(argsList[1]) - 1;
                     string saveName = String.Join(" ", argsList[2..]);
 
                     if(!this.Configuration.Hotbars.ContainsKey(saveName)) {
-                        this.ChatGui.PrintError("No hotbar saved with that name.");
+                        this.ChatGui.PrintError("No hotbar saved with the name \"" + saveName + "\".");
                         return;
                     }
 
-                    this.SetHotbar(hotbarNumber, this.Configuration.Hotbars[saveName]);
-                } else {
-                    this.ChatGui.PrintError("Invalid command. Usage: /vhe [save | load]");
+                    HotbarSlotSave[] hotbar = this.Configuration.Hotbars[saveName];
+                    for (uint i = 0; i < HOTBAR_SIZE; i++) {
+                        HotBarSlot* gameSlot = raptureHotbarModule->GetSlotById(hotbarNumber, i);
+                        gameSlot->Set(hotbar[i].CommandType, hotbar[i].CommandId);
+                        raptureHotbarModule->WriteSavedSlot(raptureHotbarModule->ActiveHotbarClassJobId, hotbarNumber, i, gameSlot, false, ClientState.IsPvP);
+                    }
+                } else if(argsList[0] == CLEAR_COMMAND) {
+                    if(!validateArguments(argsList)) {
+                        return;
+                    }
+
+                    uint hotbarNumber = uint.Parse(argsList[1]) - 1;
+
+                    for (uint i = 0; i < HOTBAR_SIZE; i++) {
+                        HotBarSlot* gameSlot = raptureHotbarModule->GetSlotById(hotbarNumber, i);
+                        gameSlot->Set(HotbarSlotType.Empty, 0);
+                        raptureHotbarModule->WriteSavedSlot(raptureHotbarModule->ActiveHotbarClassJobId, hotbarNumber, i, gameSlot, false, ClientState.IsPvP);
+                    }
                 }
             }
         }
@@ -143,7 +154,7 @@ namespace VanillaHotbarExtender {
         }
 
         public void DrawConfigUI() {
-            WindowSystem.GetWindow(ConfigWindow.NAME)!.IsOpen = true;
+            configWindow.IsOpen = true;
         }
     }
 }
